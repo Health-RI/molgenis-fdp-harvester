49c1b4bfbd2d61377f9059b12eedf4724d8c2744
Alexander Harms <alexander.harms@health-ri.nl>
2025-02-28 13:12:48 +0100

    refactor: Refactor MolgenisEUCAIMDCATAPProfile

--- src/molgenis_fdp_harvester/ckan_harvest/molgenis_dcat_profile.py
+++ src/molgenis_fdp_harvester/ckan_harvest/molgenis_dcat_profile.py
@@ -8,7 +8,7 @@
 #
 # Modified by Stichting Health-RI to remove dependencies on CKAN
 
-from typing import Dict
+from typing import Dict, Union
 
 from rdflib import URIRef, FOAF
 from yarl import URL
@@ -50,13 +50,34 @@ class MolgenisEUCAIMDCATAPProfile(RDFProfile):
     https://joinup.ec.europa.eu/asset/dcat_application_profile
 
     """
+    def _extract_name_from_query(self, value: Union[list, str]):
+        if isinstance(value, list):
+            value = [URL(val).query.get('name') for val in value]
+        else:
+            value = URL(value).query.get('name')
+        return value
+
+    def _extract_concept_dict(self, concept_ref, concept_dict: Dict, key_predicate_tuple: tuple, query_property_list: list):
+        for key, predicate in key_predicate_tuple:
+            value = self._object_value(concept_ref, predicate)
+            if len(value) == 1:
+                value = value[0]
+            if value:
+                if key in query_property_list:
+                    value = self._extract_name_from_query(value)
+                if isinstance(value, list):
+                    value = ",".join(value)
+                concept_dict[key] = value
+        return concept_dict
 
     def parse_dataset(self, dataset_dict: Dict, dataset_ref: URIRef):
         # dataset_dict["extras"] = []
         # dataset_dict["resources"] = []
         dataset_dict["uri"] = str(dataset_ref)
         # Basic fields
-        for key, predicate in (
+        query_property_list = ['order_of_magnitude', 'imaging_modality','geographical_coverage','type',
+                               'image_access_type', 'collection_method']
+        key_predicate_tuple = (
             # ("id", DCT.identifier),
             ("name", DCT.title),
             ("description", DCT.description),
@@ -70,10 +91,8 @@ class MolgenisEUCAIMDCATAPProfile(RDFProfile):
             ("image_access_type",
              URIRef("http://catalogue-eucaim.grycap.i3m.upv.es/Eucaim/api/rdf/Collections/column/image_access_type")),
             ("collection_method", URIRef("http://catalogue-eucaim.grycap.i3m.upv.es/Eucaim/api/rdf/Collections/column/collection_method"))
-        ):
-            value = self._object_value(dataset_ref, predicate)
-            if value:
-                dataset_dict[key] = value
+        )
+        dataset_dict = self._extract_dataset_dict(dataset_ref, dataset_dict, key_predicate_tuple, query_property_list)
 
         # TODO store keywords somewhere
         # replace munge_tag to noop if there's no need to clean tags
@@ -89,10 +108,7 @@ class MolgenisEUCAIMDCATAPProfile(RDFProfile):
 
         # dataset_dict["biobank"] = URL(dataset_dict["biobank"]).query.get('id')
         dataset_dict["biobank"] = munge_title_to_name(dataset_dict["biobank"])
-        query_property_list = ['order_of_magnitude', 'imaging_modality','geographical_coverage','type',
-                               'image_access_type', 'collection_method']
-        for query_property in query_property_list:
-           dataset_dict[query_property] = URL(dataset_dict[query_property]).query.get('name')
+
 
         return dataset_dict
 
@@ -101,16 +117,19 @@ class MolgenisEUCAIMDCATAPProfile(RDFProfile):
         # dataset_dict["resources"] = []
         dataset_dict["uri"] = str(dataset_ref)
         # Basic fields
-        for key, predicate in (
-                # ("id", DCT.identifier),
-                ("name", DCT.title),
-                ("description", DCT.description),
-                ("geographical_coverage", DCT.spatial),
-                ("juridical_person", URIRef("http://catalogue-eucaim.grycap.i3m.upv.es/Eucaim/api/rdf/Biobanks/column/juridical_person")),
-        ):
-            value = self._object_value(dataset_ref, predicate)
-            if value:
-                dataset_dict[key] = value
+        key_predicate_tuple = (
+            # ("id", DCT.identifier),
+            ("name", DCT.title),
+            ("description", DCT.description),
+            ("geographical_coverage", DCT.spatial),
+            ("juridical_person", DCT.publisher),
+            ("url", DCAT.landingPage),
+            ("contact", DCAT.contactPoint),
+            ("network", URIRef("http://catalogue-eucaim.grycap.i3m.upv.es/Eucaim/api/rdf/Biobanks/column/network")),
+            ("withdrawn", URIRef("http://catalogue-eucaim.grycap.i3m.upv.es/Eucaim/api/rdf/Biobanks/column/withdrawn")),
+        )
+        query_property_list = ['geographical_coverage']
+        dataset_dict = self._extract_dataset_dict(dataset_ref, dataset_dict, key_predicate_tuple, query_property_list)
 
         # # TODO store keywords somewhere
         # # replace munge_tag to noop if there's no need to clean tags
@@ -124,9 +143,11 @@ class MolgenisEUCAIMDCATAPProfile(RDFProfile):
         # # These values are fake. They need to be made "real"
         # # log.warning("Filling in fake values")
 
-        query_property_list = ['geographical_coverage']
-        for query_property in query_property_list:
-            dataset_dict[query_property] = URL(dataset_dict[query_property]).query.get('name')
+        try:
+            dataset_dict["contact"] = URL(dataset_dict["contact"]).query.get('id')
+        except KeyError:
+            pass
+        dataset_dict["network"] = URL(dataset_dict["network"]).query.get('id')
 
         return dataset_dict
 
@@ -135,21 +156,17 @@ class MolgenisEUCAIMDCATAPProfile(RDFProfile):
         # dataset_dict["resources"] = []
         dataset_dict["uri"] = str(dataset_ref)
         # Basic fields
-        for key, predicate in (
-                ("id", FOAF.openid),
-                ("name", FOAF.openid),
-                ("email", FOAF.mbox),
-                ("first_name", FOAF.firstName),
-                ("last_name", FOAF.lastName),
-                ("country", URIRef("http://catalogue-eucaim.grycap.i3m.upv.es/Eucaim/api/rdf/Persons/column/country")),
-        ):
-            value = self._object_value(dataset_ref, predicate)
-            if value:
-                dataset_dict[key] = value
-
+        key_predicate_tuple = (
+            ("id", FOAF.openid),
+            ("name", FOAF.openid),
+            ("email", FOAF.mbox),
+            ("first_name", FOAF.firstName),
+            ("last_name", FOAF.lastName),
+            ("country", URIRef("http://catalogue-eucaim.grycap.i3m.upv.es/Eucaim/api/rdf/Persons/column/country")),
+        )
         query_property_list = ['country']
-        for query_property in query_property_list:
-            dataset_dict[query_property] = URL(dataset_dict[query_property]).query.get('name')
+        dataset_dict = self._extract_dataset_dict(dataset_ref, dataset_dict, key_predicate_tuple, query_property_list)
+
         dataset_dict["email"] = dataset_dict["email"].removeprefix("mailto:")
         return dataset_dict
 
