5f8050025ca99800efdee2d854661c6c78304084
SeanBerrieHRI <sean.berrie@health-ri.nl>
2026-08-11 11:24:02 +0200

    feat: add parser for prohibition according EUCAIM DCAT AP v1.0

--- src/molgenis_fdp_harvester/base/molgenis_dcat_profile.py
+++ src/molgenis_fdp_harvester/base/molgenis_dcat_profile.py
@@ -482,6 +482,15 @@ class MolgenisEUCAIMDCATAPProfile(RDFProfile):
         dataset_dict = self._extract_concept_dict(dataset_ref, dataset_dict, key_predicate_tuple)
         return dataset_dict
 
+    def parse_prohibition(self, dataset_dict: Dict, dataset_ref: URIRef):
+        dataset_dict["uri"] = str(dataset_ref)
+        key_predicate_tuple = (
+            ("id", uuid.uuid4()),
+            ("action", ODRL.action),
+        )
+        dataset_dict = self._extract_concept_dict(dataset_ref, dataset_dict, key_predicate_tuple)
+        return dataset_dict
+
     def graph_from_dataset(self, dataset_dict, dataset_ref):
         raise NotImplementedError("FDP export is handled by MOLGENIS")
 
